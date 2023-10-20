ceb677822b31d531beb7d60f6fa2740443f30408
Volodymyr Herashchenko <cholkegn@gmail.com>
2023-10-20 15:43:39 +0300

    moved Generator.cs into main project directory

---
+++ LongArithmetic/LongArithmetic/Generator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LongArithmetic
+{
+    public static class Generator
+    {
+        public static string GenerateNewHex(int n)
+        {
+            var rand = new Random();
+            var output = "";
+            for(int i =0 ; i < n - 1; i++)
+            {
+                var r = rand.Next(0,15);
+                output += Convertor.DigitIntoHexSymbol((uint)r);
+            }
+            output += Convertor.DigitIntoHexSymbol((uint)rand.Next(1, 15));
+            return output;
+        }
+    }
+}
