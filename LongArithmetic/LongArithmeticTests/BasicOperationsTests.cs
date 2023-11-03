1f6711288782066c5054c4284396aae10156aa8e
Volodymyr Herashchenko <cholkegn@gmail.com>
2023-11-03 12:51:36 +0200

    changed tests, added test for gcd and lcm

--- LongArithmetic/LongArithmeticTests/BasicOperationsTests.cs
+++ LongArithmetic/LongArithmeticTests/BasicOperationsTests.cs
@@ -6,8 +6,7 @@ namespace LongArithmeticTests
 {
     public class Tests
     {
-        string number1;
-        string number2;
+        LongInt n1; LongInt n2; BigInteger n3; BigInteger n4;
         string power;
         int powerOf = 0;
 
@@ -15,103 +14,74 @@ namespace LongArithmeticTests
         public void Setup()
         {
             var rand = new Random();
-            var r = rand.Next(150, 200);
-            var p = rand.Next(1,10);
+            var r = rand.Next(30, 250);
+            var p = rand.Next(8,15);
             powerOf = rand.Next(5, 10);
             power = "0" + Generator.GenerateNewHex(rand.Next(60, 90));
-            number1 += ("0" + Generator.GenerateNewHex(r));
-            number2 += ("0" + Generator.GenerateNewHex(r - p));
-            
+            var number1 = "0" + Generator.GenerateNewHex(r);
+            var number2 = "0" + Generator.GenerateNewHex(r - p);
+
+
+            n1 = new LongInt(number1);
+            n2 = new LongInt(number2);
+            n3 = BigInteger.Parse(number1, NumberStyles.HexNumber);
+            n4 = BigInteger.Parse(number2, NumberStyles.HexNumber);
+
         }
 
         [Test]
         public void Addition()
         {
-            var n1 = new LongInt(number1);
-            var n2 = new LongInt(number2);
-
-            var n3 = BigInteger.Parse(number1, NumberStyles.HexNumber);
-            var n4 = BigInteger.Parse(number2, NumberStyles.HexNumber);
-
             var out1 = n1 + n2;
             var out2 = n3 + n4;
             var strOut1 = out1.ToString();
             var strOut2 = out2.ToString("X");
             strOut2 = strOut2.TrimStart('0');
-            Assert.IsTrue(strOut1 == strOut2);
+            Assert.That(strOut1, Is.EqualTo(strOut2));
         }
         [Test]
         public void Substraction()
         {
-            var n1 = new LongInt(number1);
-            var n2 = new LongInt(number2);
-
-            var n3 = BigInteger.Parse(number1, NumberStyles.HexNumber);
-            var n4 = BigInteger.Parse(number2, NumberStyles.HexNumber);
-
             var out1 = n1 - n2;
             var out2 = n3 - n4;
             var strOut1 = out1.ToString();
             var strOut2 = out2.ToString("X");
             strOut2 = strOut2.TrimStart('0');
-            Assert.IsTrue(strOut1 == strOut2);
+            Assert.That(strOut1, Is.EqualTo(strOut2));
         }
         [Test]
         public void Multiply()
         {
-            var n1 = new LongInt(number1);
-            var n2 = new LongInt(number2);
-
-            var n3 = BigInteger.Parse(number1, NumberStyles.HexNumber);
-            var n4 = BigInteger.Parse(number2, NumberStyles.HexNumber);
-
             var out1 = n1 * n2;
             var out2 = n3 * n4;
             var strOut1 = out1.ToString();
             var strOut2 = out2.ToString("X");
             strOut2 = strOut2.TrimStart('0');
-            Assert.IsTrue(strOut1 == strOut2);
+            Assert.That(strOut1, Is.EqualTo(strOut2));
         }
         [Test]
         public void Divide()
         {
-            var n1 = new LongInt(number1);
-            var n2 = new LongInt(number2);
-
-            var n3 = BigInteger.Parse(number1, NumberStyles.HexNumber);
-            var n4 = BigInteger.Parse(number2, NumberStyles.HexNumber);
-
             var out1 = n1 / n2;
             var out2 = n3 / n4;
             var strOut1 = out1.ToString();
             var strOut2 = out2.ToString("X");
             strOut2 = strOut2.TrimStart('0');
-            Assert.IsTrue(strOut1 == strOut2);
+            Assert.That(strOut1, Is.EqualTo(strOut2));
         }
         [Test]
         public void Modulo()
         {
-            var n1 = new LongInt(number1);
-            var n2 = new LongInt(number2);
-
-            var n3 = BigInteger.Parse(number1, NumberStyles.HexNumber);
-            var n4 = BigInteger.Parse(number2, NumberStyles.HexNumber);
-
             var out1 = n1 % n2;
             var out2 = n3 % n4;
             var strOut1 = out1.ToString();
             var strOut2 = out2.ToString("X");
             strOut2 = strOut2.TrimStart('0');
-            Assert.IsTrue(strOut1 == strOut2);
+            Assert.That(strOut1, Is.EqualTo(strOut2));
         }
         [Test]
         public void PowerOf()
         {
-            var n1 = new LongInt(power);
-
-
-            var n3 = BigInteger.Parse(power, NumberStyles.HexNumber);
-
 
             var out1 = LongInt.Pow(n1,new LongInt((uint)powerOf));
             var out2 = BigInteger.Pow(n3, powerOf);
@@ -119,7 +89,26 @@ namespace LongArithmeticTests
             var strOut1 = out1.ToString();
             var strOut2 = out2.ToString("X");
             strOut2 = strOut2.TrimStart('0');
-            Assert.IsTrue(strOut2.Contains(strOut1));
+            Assert.That(strOut1, Is.EqualTo(strOut2));
+        }
+        [Test]
+        public void GCD()
+        {
+            var out1 = LongInt.BinaryGCD(n1, n2);
+            var out2 = BigInteger.GreatestCommonDivisor(n3, n4);
+            var strOut1 = out1.ToString();
+            var strOut2 = out2.ToString("X");
+            Assert.That(strOut1, Is.EqualTo(strOut2));
+        }
+        [Test]
+        public void LCM()
+        {
+            var out1 = LongInt.LCM(n1, n2);
+            var out2 = n3 * n4 / BigInteger.GreatestCommonDivisor(n3, n4);
+            var strOut1 = out1.ToString();
+            var strOut2 = out2.ToString("X");
+            strOut2 = strOut2.TrimStart('0');
+            Assert.That(strOut1, Is.EqualTo(strOut2));
         }
 
     }
